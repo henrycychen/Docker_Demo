dafc1f902924c90a6b37f4b3194db8817db44890
Henry <henrycychen@gmail.com>
2018-02-06 09:54:48 -0800

    Adding maven files and sample test

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SearchTest {
+
+	@Test
+	public void test() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a+b == 3);
+	}
+
+}
